73f39adc976f306a6d699cdee48dfb368b6f99c2
GitHub Action <action@github.com>
2021-11-17 09:18:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kevinlin/intellijplugincommitmessage/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kevinlin.intellijplugincommitmessage.services
+
+import com.github.kevinlin.intellijplugincommitmessage.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
